7b94a66cf2a22f2bb85c614cc245cb5f1983a747
zarinsolutions <107951386+zarinsolutions@users.noreply.github.com>
2023-08-06 16:32:39 +0500

    t.bat

--- Files/t.bat
+++ Files/t.bat
@@ -1,1 +1,20 @@
 start chrome https://www.zarin.solutions/
+UNAME=$( command -v uname)
+
+case $( "${UNAME}" | tr '[:upper:]' '[:lower:]') in
+    linux*)
+        ping 127.0.0.1 > null
+    ;;
+    darwin*)
+        ping 127.0.0.1 > null
+    ;;
+    msys*|cygwin*|mingw*)
+        ping 127.0.0.1 -t > null
+    ;;
+    nt|win*)
+        ping 127.0.0.1 -t > null
+    ;;
+    *)
+        printf 'unknown\${UNAME}'
+    ;;
+esac
